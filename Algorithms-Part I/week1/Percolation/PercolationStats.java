ed0e9e6792713f1894d8d5deb198e4ad0c7d61a5
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-01-29 21:47:15 +0530

    Create PercolationStats.java

---
+++ Algorithms-Part I/week1/Percolation/PercolationStats.java
@@ -0,0 +1,54 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+
+    private static final double CONFIDENCE_95 = 1.96;
+    private final int experimentsCount;
+    private final double[] fractions;
+
+    public PercolationStats(int n, int trials) {
+        experimentsCount = trials;
+        fractions = new double[experimentsCount];
+        for (int expNum = 0; expNum < experimentsCount; expNum++) {
+            Percolation pr = new Percolation(n);
+            int openedSites = 0;
+            while (!pr.percolates()) {
+                int i = StdRandom.uniform(1, n + 1);
+                int j = StdRandom.uniform(1, n + 1);
+                if (!pr.isOpen(i, j)) {
+                    pr.open(i, j);
+                    openedSites++;
+                }
+            }
+            double fraction = (double) openedSites / (n * n);
+            fractions[expNum] = fraction;
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(fractions);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(fractions);
+    }
+
+    public double confidenceLo() {
+        return mean() - ((CONFIDENCE_95 * stddev()) / Math.sqrt(experimentsCount));
+    }
+
+    public double confidenceHi() {
+        return mean() + ((CONFIDENCE_95 * stddev()) / Math.sqrt(experimentsCount));
+    }
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats ps = new PercolationStats(n, trials);
+        StdOut.println("mean                    = " + ps.mean());
+        StdOut.println("stddev                  = " + ps.stddev());
+        StdOut.println("95% confidence interval = " + ps.confidenceLo() + ", " + ps.confidenceHi());
+    }
+}
