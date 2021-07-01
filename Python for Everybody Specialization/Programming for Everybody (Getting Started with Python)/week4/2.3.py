baf8b762ae14f6cecc241a477d181d49bab6ddff
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-07-01 13:53:50 +0530

    Create 2.3.py

---
+++ Python for Everybody Specialization/Programming for Everybody (Getting Started with Python)/week4/2.3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on Jun 15 12:29 PM IST
+@author: 10adnan75
+"""
+
+hrs = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+cost = float(hrs) * float(rate)
+
+print("Pay: %s" % cost)
