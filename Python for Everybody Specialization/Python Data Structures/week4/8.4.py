2970b5dce6dfde64f88ab9d01f2307352805314d
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-07-01 14:35:55 +0530

    Create 8.4.py

---
+++ Python for Everybody Specialization/Python Data Structures/week4/8.4.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on Jun 15 12:29 PM IST
+@author: 10adnan75
+"""
+
+get = input('Please enter file name: ')
+handle = open(get)
+text = list()
+for line in handle:
+	line = line.rstrip()
+	line = line.split()
+	for i in line:
+		if i in text:
+			continue
+		else:
+			text.append(i)
+text.sort()
+print(text)
