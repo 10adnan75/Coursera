04dd6cc6abf52b250c85184273ea5a9965a8382a
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-07-01 14:25:36 +0530

    Create 6.5.py

---
+++ Python for Everybody Specialization/Python Data Structures/week1/6.5.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on May 25 12:29 PM IST
+@author: 10adnan75
+"""
+
+text = "X-DSPAM-Confidence:    0.8475";
+a = text[-6:]
+b = float(a)
+print(b)
+text.find(":")
