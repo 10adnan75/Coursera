b014b3f4e2c1484b934b4d2ff2a9701cb1e8ce8d
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-07-01 14:44:37 +0530

    Update 9.4.py

--- Python for Everybody Specialization/Python Data Structures/week5/9.4.py
+++ Python for Everybody Specialization/Python Data Structures/week5/9.4.py
@@ -10,21 +10,21 @@ handle = open(file)
 data1 = list()
 data2 = dict()
 for line in handle:
-	line = line.rstrip()
-	if not line.startswith('From '):
-		continue
-	line = line.split()
-	line = line[1]
-	data1.append(line)
+  line = line.rstrip()
+  if not line.startswith('From '):
+    continue
+  line = line.split()
+  line = line[1]
+  data1.append(line)
 for i in data1:
-	data2[i] = data2.get(i, 0) + 1
+  data2[i] = data2.get(i, 0) + 1
 
 word = None
 max = None
 
 for aa, bb in data2.items():
-	if max is None or bb > max:
-		word = aa
-		max = bb
+  if max is None or bb > max:
+    word = aa
+    max = bb
 
 print(word, max)
