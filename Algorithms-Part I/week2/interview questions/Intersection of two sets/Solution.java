5781925bc26f1d1bb414b449138499958b566bd0
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-07-30 21:05:51 +0530

    Update Solution.java

--- Algorithms-Part I/week2/interview questions/Intersection of two sets/Solution.java
+++ Algorithms-Part I/week2/interview questions/Intersection of two sets/Solution.java
@@ -54,7 +54,6 @@ class Solution {
     }
     
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
         System.out.print("How many points?: ");
         int n = input.nextInt();
@@ -82,3 +81,40 @@ class Solution {
         System.out.println("A intersection B: " + countIntersection(a, b));
     }
 }
+
+/*      How many points?: 5
+        Input values for Set A and Set B: 
+        A.x1: 0
+        A.y1: 1
+        B.x1: 2
+        B.y1: 3
+        A.x2: 4
+        A.y2: 5
+        B.x2: 6
+        B.y2: 7
+        A.x3: 8
+        A.y3: 9
+        B.x3: 0
+        B.y3: 1
+        A.x4: 2
+        A.y4: 3
+        B.x4: 4
+        B.y4: 5
+        A.x5: 6
+        A.y5: 7
+        B.x5: 8
+        B.y5: -1
+        A: 
+        0 1
+        2 3
+        4 5
+        6 7
+        8 9
+        B: 
+        0 1
+        2 3
+        4 5
+        6 7
+        8 -1
+        A intersection B: 4
+*/
