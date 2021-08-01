deb7d77935632c52b011babf2612761471a2d5a7
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-08-01 20:32:26 +0530

    Update Solution.java

--- Algorithms-Part I/week2/interview questions/Permutation/Solution.java
+++ Algorithms-Part I/week2/interview questions/Permutation/Solution.java
@@ -2,30 +2,46 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 class Solution {
+    
+    public static boolean isPermutation(int[] a, int[] b) {
+        if (a.length != b.length) {
+            return false;
+        }
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for (int i=0; i<a.length; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter size: ");
-        int n = input.nextInt();
-        int[] a = new int[n], b = new int[n];
+        int aSize = input.nextInt();
+        int[] a = new int[aSize]; 
         System.out.println("Enter 1st array elements: ");
-        for (int i=0; i<n; i++) a[i] = input.nextInt();
+        for (int i=0; i<aSize; i++) a[i] = input.nextInt();
+        System.out.print("Enter size: ");
+        int bSize = input.nextInt();
+        int[] b = new int[bSize];
         System.out.println("Enter 2nd array elements: ");
-        for (int i=0; i<n; i++) b[i] = input.nextInt();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        for (int i=0; i<n; i++) {
-            if (a[i]!=b[i]) {
-                System.out.println("No");
-                System.exit(0);
-            }
+        for (int i=0; i<bSize; i++) b[i] = input.nextInt();
+        
+        if (isPermutation(a, b)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
         }
-        System.out.println("Yes");
     }
 }
 
 /*      OUTPUT:
         Enter size: 5
         Enter 1st array elements: 1 2 3 4 5
-        Enter 2nd array elements: 5 3 2 1 4
-        Yes
+        Enter size: 6
+        Enter 2nd array elements: 5 3 2 1 4 0
+        No
 */
