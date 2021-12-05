e28a2f1f6a30b57e27929dc219712c2cbe5ece5d
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-12-05 14:19:18 +0530

    Add files via upload

---
+++ Open Source Software Development, Linux and Git Specialization/Linux for Developers/week1/lab/apropos_stat_out.sh
@@ -0,0 +1,130 @@
+aa-status (8)        - display various information about the current AppArmor...
+aio_error (3)        - get error status of asynchronous I/O operation
+aio_return (3)       - get return status of asynchronous I/O operation
+apparmor_status (8)  - display various information about the current AppArmor...
+aptitude-create-state-bundle (1) - bundle the current aptitude state
+aptitude-run-state-bundle (1) - unpack an aptitude state bundle and invoke ap...
+arch_prctl (2)       - set architecture-specific thread state
+avahi.hosts (5)      - avahi-daemon static host name file
+avahi.service (5)    - avahi-daemon static service file
+bounce (8postfix)    - Postfix delivery status reports
+clearerr (3)         - check and reset stream status
+ctstat (8)           - unified linux network statistics
+cups-lpd (8)         - receive print jobs and report printer status to lpd cl...
+defer (8postfix)     - Postfix delivery status reports
+devlink-monitor (8)  - state monitoring
+diffstat (1)         - make histogram from diff-output
+dmstats (8)          - device-mapper statistics management
+dpkg-statoverride (1) - override ownership and mode of files
+Dpkg::Deps::Simple (3perl) - represents a single dependency statement
+e2mmpstatus (8)      - Check MMP status of an ext4 filesystem
+fdatasync (2)        - synchronize a file's in-core state with storage device
+feof (3)             - check and reset stream status
+ferror (3)           - check and reset stream status
+fileno (3)           - check and reset stream status
+Font::TTF::Kern::StateTable (3pm) - State Table Kern subtable for AAT
+fstab (5)            - static information about the filesystems
+fstat (2)            - get file status
+fstat64 (2)          - get file status
+fstatat (2)          - get file status
+fstatat64 (2)        - get file status
+fstatfs (2)          - get filesystem statistics
+fstatfs64 (2)        - get filesystem statistics
+fstatvfs (3)         - get filesystem statistics
+fsync (2)            - synchronize a file's in-core state with storage device
+git-reset (1)        - Reset current HEAD to the specified state
+git-status (1)       - Show the working tree status
+gnome-power-statistics (1) - gnome power statistics gui
+gst-stats-1.0 (1)    - print info gathered from a GStreamer log file
+hosts (5)            - static table lookup for hostnames
+HTTP::Status (3pm)   - HTTP Status code processing
+iecset (1)           - Set or dump IEC958 status bits
+initstate (3)        - random number generator
+initstate_r (3)      - reentrant random number generator
+ip-l2tp (8)          - L2TPv3 static unmanaged tunnel configuration
+ip-monitor (8)       - state monitoring
+iwspy (8)            - Get wireless statistics from specific nodes
+jaotc (1)            - The Java static compiler that produces native code for...
+jdeprscan (1)        - static analysis tool that scans a jar file (or some ot...
+jshell (1)           - interactively evaluate declarations, statements, and e...
+jstat (1)            - monitor JVM statistics
+jstatd (1)           - monitor the creation and termination of instrumented J...
+kbdinfo (1)          - obtain information about the status of a console
+lintian (1)          - Static analysis tool for Debian packages
+Lintian::Processable::Diffstat (3) - access to collected diffstat data
+lnstat (8)           - unified linux network statistics
+lpq (1)              - show printer queue status
+lpstat (1)           - print cups status information
+lsmem (1)            - list the ranges of available memory with their online ...
+lsmod (8)            - Show the status of modules in the Linux Kernel
+lstat (2)            - get file status
+lstat64 (2)          - get file status
+malloc_get_state (3) - record and restore state of malloc implementation
+malloc_info (3)      - export malloc state to a stream
+malloc_set_state (3) - record and restore state of malloc implementation
+malloc_stats (3)     - print memory allocation statistics
+mbsinit (3)          - test for initial shift state
+memusagestat (1)     - generate graphic from memory profiling data
+mii-tool (8)         - view, manipulate media-independent interface status
+netstat (8)          - Print network connections, routing tables, interface s...
+networkctl (1)       - Query the status of network links
+networkd-dispatcher (8) - Dispatcher service for systemd-networkd connection ...
+newfstatat (2)       - get file status
+nstat (8)            - network statistics tools.
+ocsp (1ssl)          - Online Certificate Status Protocol utility
+oldfstat (2)         - get file status
+oldlstat (2)         - get file status
+oldstat (2)          - get file status
+openssl-ocsp (1ssl)  - Online Certificate Status Protocol utility
+pam_sepermit (8)     - PAM module to allow/deny login depending on SELinux en...
+pppstats (8)         - print PPP statistics
+prtstat (1)          - print statistics of a process
+pthread_attr_getdetachstate (3) - set/get detach state attribute in thread at...
+pthread_attr_setdetachstate (3) - set/get detach state attribute in thread at...
+pthread_setcancelstate (3) - set cancelability state and type
+pthread_setcanceltype (3) - set cancelability state and type
+rdma-statistic (8)   - RDMA statistic counter configuration
+rtacct (8)           - network statistics tools.
+rtcwake (8)          - enter a system sleep state until specified wakeup time
+rtstat (8)           - unified linux network statistics
+sched_get_priority_max (2) - get static priority range
+sched_get_priority_min (2) - get static priority range
+seccomp (2)          - operate on Secure Computing state of the process
+setstate (3)         - random number generator
+setstate_r (3)       - reentrant random number generator
+skill (1)            - send a signal or report process status
+slabinfo (5)         - kernel slab allocator statistics
+snice (1)            - send a signal or report process status
+sssctl (8)           - SSSD control and status utility
+stat (1)             - display file or file system status
+stat (2)             - get file status
+stat64 (2)           - get file status
+statfs (2)           - get filesystem statistics
+statfs64 (2)         - get filesystem statistics
+statvfs (3)          - get filesystem statistics
+statx (2)            - get file status (extended)
+systemd-hibernate.service (8) - System sleep state logic
+systemd-hybrid-sleep.service (8) - System sleep state logic
+systemd-notify (1)   - Notify service manager about start-up completion and o...
+systemd-rfkill (8)   - Load and save the RF kill switch state at boot and change
+systemd-rfkill.service (8) - Load and save the RF kill switch state at boot a...
+systemd-rfkill.socket (8) - Load and save the RF kill switch state at boot an...
+systemd-sleep (8)    - System sleep state logic
+systemd-suspend-then-hibernate.service (8) - System sleep state logic
+systemd-suspend.service (8) - System sleep state logic
+tc-nat (8)           - stateless native address translation action
+Text::Xslate::PP::State (3pm) - Text::Xslate pure-Perl virtual machine state
+timer_gettime (2)    - arm/disarm and fetch state of POSIX per-process timer
+timer_settime (2)    - arm/disarm and fetch state of POSIX per-process timer
+trace (8postfix)     - Postfix delivery status reports
+tracker-status (1)   - Provide status and statistics on the data indexed
+tracker3-status (1)  - Provide status and statistics on the data indexed
+usb_modeswitch (1)   - control the mode of 'multi-state' USB devices
+ustat (2)            - get filesystem statistics
+vmstat (8)           - Report virtual memory statistics
+wait (2)             - wait for process to change state
+wait3 (2)            - wait for process to change state, BSD style
+wait4 (2)            - wait for process to change state, BSD style
+waitid (2)           - wait for process to change state
+waitpid (2)          - wait for process to change state
+wdctl (8)            - show hardware watchdog status
