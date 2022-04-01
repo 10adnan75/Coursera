ba472dcc915ec7698bb3795ecc768a92cddc924a
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2022-04-01 21:34:25 +0530

    Update 3_car_fueling.cpp

--- Data Structures and Algorithms Specialization/Algorithmic Toolbox/week3/3_car_fueling.cpp
+++ Data Structures and Algorithms Specialization/Algorithmic Toolbox/week3/3_car_fueling.cpp
@@ -7,46 +7,46 @@ using std::vector;
 using std::max;
 
 int compute_min_refills(int dist, int tank, vector<int> & stops) {
-  if (dist <= tank) {
-    return 0;
-  }
-  int i = 0;
-  int num_stops = 0;
-  int current_stop = 0;
-  int dist_covered = 0;
-  bool flag = false;
-  stops.push_back(dist);
-  while (dist_covered < dist) {
-    while (i < stops.size() and stops[i] - current_stop <= tank) {
-      i++;
-      flag = true;
+    if (dist <= tank) {
+        return 0;
     }
-    if (flag) {
-      if (i == stops.size()) {
-        break;
-      }
-      current_stop = stops[i - 1];
-      dist_covered = current_stop;
-      num_stops += 1;
-    } else {
-      return -1;
+    int i = 0;
+    int num_stops = 0;
+    int current_stop = 0;
+    int dist_covered = 0;
+    bool flag = false;
+    stops.push_back(dist);
+    while (dist_covered < dist) {
+        while (i < stops.size() and stops[i] - current_stop <= tank) {
+            i++;
+            flag = true;
+        }
+        if (flag) {
+            if (i == stops.size()) {
+                break;
+            }
+            current_stop = stops[i - 1];
+            dist_covered = current_stop;
+            num_stops += 1;
+        } else {
+            return -1;
+        }
+        flag = false;
     }
-    flag = false;
-  }
-  return num_stops;
+    return num_stops;
 }
 
 int main() {
-  int d = 0;
-  cin >> d;
-  int m = 0;
-  cin >> m;
-  int n = 0;
-  cin >> n;
-  vector<int> stops(n);
-  for (size_t i = 0; i < n; ++i) {
-    cin >> stops.at(i);
-  }
-  cout << compute_min_refills(d, m, stops) << "\n";
-  return 0;
+    int d = 0;
+    cin >> d;
+    int m = 0;
+    cin >> m;
+    int n = 0;
+    cin >> n;
+    vector<int> stops(n);
+    for (size_t i = 0; i < n; ++i) {
+        cin >> stops.at(i);
+    }
+    cout << compute_min_refills(d, m, stops) << "\n";
+    return 0;
 }
