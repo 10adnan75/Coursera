20d53577dbca41c194bc89d98e8175cb3637c413
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2022-04-03 17:55:36 +0530

    Update 4_longest_common_subsequence_of_two_sequences.cpp

--- Data Structures and Algorithms Specialization/Algorithmic Toolbox/week5/4_longest_common_subsequence_of_two_sequences.cpp
+++ Data Structures and Algorithms Specialization/Algorithmic Toolbox/week5/4_longest_common_subsequence_of_two_sequences.cpp
@@ -6,26 +6,26 @@ using std::vector;
 
 int lcs2(vector<int> &a, vector<int> &b) {
     int sequence[a.size() + 1][b.size() + 1];
-	int index = 0;
-	while (index <= a.size()) {
-		sequence[index][0] = 0;
-		index++;
-	}
-	index = 1;
-	while (index <= b.size()) {
-		sequence[0][index] = 0;
-		index++;
-	}
-	for (size_t i = 1; i <= a.size(); i++) {
-		for (size_t j = 1; j <= b.size(); j++) {
-			if (a.at(i - 1) == b.at(j - 1)) {
-				sequence[i][j] = 1 + sequence[i - 1][j - 1];
-			} else {
-				sequence[i][j] = std::max(sequence[i - 1][j], sequence[i][j - 1]);
-			}
-		}
-	}
-	return sequence[a.size()][b.size()];
+    int index = 0;
+    while (index <= a.size()) {
+        sequence[index][0] = 0;
+        index++;
+    }
+    index = 1;
+    while (index <= b.size()) {
+        sequence[0][index] = 0;
+        index++;
+    }
+    for (size_t i = 1; i <= a.size(); i++) {
+        for (size_t j = 1; j <= b.size(); j++) {
+            if (a.at(i - 1) == b.at(j - 1)) {
+                sequence[i][j] = 1 + sequence[i - 1][j - 1];
+            } else {
+                sequence[i][j] = std::max(sequence[i - 1][j], sequence[i][j - 1]);
+            }
+        }
+    }
+    return sequence[a.size()][b.size()];
 }
 
 int main() {
