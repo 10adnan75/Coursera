0071c099ab689cbf90d181e4fba3e41545018327
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2022-04-03 18:12:42 +0530

    Update 3_maximum_value_of_an_arithmetic_expression.cpp

--- Data Structures and Algorithms Specialization/Algorithmic Toolbox/week6/3_maximum_value_of_an_arithmetic_expression.cpp
+++ Data Structures and Algorithms Specialization/Algorithmic Toolbox/week6/3_maximum_value_of_an_arithmetic_expression.cpp
@@ -25,40 +25,40 @@ long long eval(long long a, long long b, char op) {
 
 long long get_maximum_value(const string &exp) {
     size_t n = (exp.size() + 1) / 2;
-	vector<pair<long long, long long>> solution(n * n);
-	auto sol_get = [&solution, &exp, n](long long i, long long j) {
-		return solution.at(i * n + j);
-	};
-	auto sol_set = [&solution, &exp, n](long long i, long long j, pair<long long, long long> value) {
-		return solution[i * n + j] = value;
-	};
-	auto digits = [&exp](long long i) { 
+    vector<pair<long long, long long>> solution(n * n);
+    auto sol_get = [&solution, &exp, n](long long i, long long j) {
+        return solution.at(i * n + j);
+    };
+    auto sol_set = [&solution, &exp, n](long long i, long long j, pair<long long, long long> value) {
+        return solution[i * n + j] = value;
+    };
+    auto digits = [&exp](long long i) { 
         return exp.at(2 * i) - '0'; 
     };
-	auto op = [&exp](long long i) { 
+    auto op = [&exp](long long i) { 
         return exp.at(2 * i + 1); 
     };
-	for (size_t i = 0; i < n; i++) {
-		sol_set(i, i, make_pair(digits(i), digits(i)));
-	}
-	long long temp_min, temp_max;
-	long long a, b, c, d;
-	for (size_t i = 1; i < n; i++) {
-		for (size_t j = i; j < n; j++) {
-			temp_min = numeric_limits<long long>::max();
-			temp_max = numeric_limits<long long>::min();
-			for (size_t k = j - i; k < j; k++) {
-				a = eval(sol_get(j - i, k).first, sol_get(k + 1, j).first, op(k));
-				b = eval(sol_get(j - i, k).second, sol_get(k + 1, j).second, op(k));
-				c = eval(sol_get(j - i, k).first, sol_get(k + 1, j).second, op(k));
-				d = eval(sol_get(j - i, k).second, sol_get(k + 1, j).first, op(k));
-				temp_min = min(temp_min, min(min(a, b), min(c, d)));
-				temp_max = max(temp_max, max(max(a, b), max(c, d)));
-				sol_set(j - i, j, make_pair(temp_min, temp_max));
-			}
-		}
-	}
-	return sol_get(0, exp.size() / 2).second;
+    for (size_t i = 0; i < n; i++) {
+        sol_set(i, i, make_pair(digits(i), digits(i)));
+    }
+    long long temp_min, temp_max;
+    long long a, b, c, d;
+    for (size_t i = 1; i < n; i++) {
+        for (size_t j = i; j < n; j++) {
+            temp_min = numeric_limits<long long>::max();
+            temp_max = numeric_limits<long long>::min();
+            for (size_t k = j - i; k < j; k++) {
+                a = eval(sol_get(j - i, k).first, sol_get(k + 1, j).first, op(k));
+                b = eval(sol_get(j - i, k).second, sol_get(k + 1, j).second, op(k));
+                c = eval(sol_get(j - i, k).first, sol_get(k + 1, j).second, op(k));
+                d = eval(sol_get(j - i, k).second, sol_get(k + 1, j).first, op(k));
+                temp_min = min(temp_min, min(min(a, b), min(c, d)));
+                temp_max = max(temp_max, max(max(a, b), max(c, d)));
+                sol_set(j - i, j, make_pair(temp_min, temp_max));
+            }
+        }
+    }
+    return sol_get(0, exp.size() / 2).second;
 }
 
 int main() {
